2af1b6ab16bf3e5db74379e429bfaec4c8f11487
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2020-05-07 15:51:43 +0200

    Add functionality to App.js
    I added Clarify API, particles for background, state management, calculating face location, button and input changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+import Clarifai from "clarifai";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Navigation from "./components/Navigation/Navigation";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+import Logo from "./components/Logo/Logo";
+import Rank from "./components/Rank/Rank";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Add apiKey from your Clarifai API account (https://docs.clarifai.com/api-guide/api-overview)
+const app = new Clarifai.App({
+  apiKey: "f693266597d149689562e8eae365caef",
+});
+
+// Particles parameters for the background 
+// (from particles react npm library )
+// More info on : https://vincentgarreau.com/particles.js/
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
+
+// Defining the constructor with the state
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+      box: {},
+      route: "signin",
+      isSignedIn: false,
+    };
+  }
+
+  // Calculating face location. We are getting data from the Clarifai with
+  // face position in procentage
+  // Then getting width and height of an image and calculating positions of
+  // the rows and columns of the face
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    // The whole image width and height
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      // Calculating procentage of the corners of the face
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  // Setting new state of the box every time it reloads with a new pic
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  };
+
+  // Set a new input state when text in input changes
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  // Seting a new state of imageUrl with the input
+  // Setting up the Clarifai model 
+  // and calculating and displaying the face box
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch((err) => console.log(err));
+  };
+
+  // Set a new state when route is changing
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
+  // Rendering all the components
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
+
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === "home" ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+        ) : route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
