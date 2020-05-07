2d0cd20d22a783c6dd794de1604d1c09323d5568
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2020-05-07 15:49:48 +0200

    Add FaceRecognition.js and css
    Show the picture from added url with styles and display a bounding-box around the face

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './FaceRecognition.css'
+
+// Show the picture from added url with styles and display a bounding-box around the face
+const FaceRecognition = ({imageUrl, box}) => {
+
+	return (
+		<div className='center'>
+			<div className='absolute mt2'>
+			<img id='inputimage' alt='' src={imageUrl} width='500px' height='auto'/>
+			<div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>
+
+			</div>
+			</div>
+		</div>
+	);
+}
+
+export default FaceRecognition;
